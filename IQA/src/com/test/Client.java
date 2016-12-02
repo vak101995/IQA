2955e5bb504627aecbd0e065277604068bcb48c7
vak101995 <vak101995@gmail.com>
2016-12-02 11:01:40 +0530

    Initial checkin of IQA

---
+++ IQA/src/com/test/Client.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Client {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("output 1");
+    }
+
+}
